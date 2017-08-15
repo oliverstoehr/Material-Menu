abd499073e9d55902757209e11c3b82ae070f3ab
Oliver Stöhr <oliver.alexander.stoehr@stud.fh-luebeck.de>
2017-08-15 13:19:08 +0200

    included js, added button for toggling the menu and changed class "material-menu" to id "material-menu"

--- material-menu.js
+++ material-menu.js
@@ -1,1 +1,24 @@
 /* JavaScript for Material Menu */
+
+// Function registers all EventListeners
+function registerMenu() {
+	var menu = document.getElementById("material-menu");
+	var toggles = document.getElementsByClassName("material-menu-toggle");
+	
+	menu.addEventListener("click", function () {
+		toggleMenu(this);
+	});
+
+	for (i = 0; i < toggles.length; i++) {
+		toggles[i].addEventListener("click", function () {
+			toggleMenu(menu);
+		});
+	}
+}
+
+// opens and closes menu
+function toggleMenu(menu) {
+	menu.classList.toggle("active");
+}
+
+window.onload = registerMenu;
